c6f85408c5af7a54177e0fecb587df59b159ce25
Calvin Tennant <calvintennant@gmail.com>
2013-10-14 19:04:31 -0400

    Cleanup

--- src/Map.js
+++ src/Map.js
@@ -2,7 +2,6 @@ var hex = hex || {};
 
 hex.Map = Class.extend({
   init: function() {
-    var self = this;
     this.paper = d3.select('body')
       .append('svg')
       .attr('width', '100%')
@@ -14,28 +13,17 @@ hex.Map = Class.extend({
     this.tileSize = hex.tileSize;
     this.bounds = hex.mapSize;
 
-    this.dragSetTransform = function() {
-      var t = self.element.attr('transform');
-      if (t) {
-        t = t.replace('translate(', '')
-        t = t.replace(')', '')
-        t = t.split(',');
-        for (var i = 0; i < t.length; i++) t[i] = parseInt(t[i]);
-      } else t = [0, 0];
-      self.transform = {x: t[0], y: t[1]};
-    }
-
     this.drag = d3.behavior.drag();
-    this.drag.on('dragstart', this.dragSetTransform);
+    this.drag.on('dragstart', this.setTransform.bind(this));
+    this.drag.on('dragend', this.setTransform.bind(this));
     this.drag.on('drag', function() {
-      var translateString = 'translate(' + (self.transform.x + d3.event.dx);
-      translateString += ', ' + (self.transform.y + d3.event.dy) + ')';
-      self.element.attr('transform', translateString);
-      self.dragSetTransform();
-    });
-    this.drag.on('dragend', this.dragSetTransform);
+      var translateString = 'translate(' + (this.transform.x + d3.event.dx);
+      translateString += ', ' + (this.transform.y + d3.event.dy) + ')';
+      this.element.attr('transform', translateString);
+      this.setTransform();
+    }.bind(this));
     this.element.call(this.drag);
-    
+
     this._mapTiles = [];
     this._tessellations = {};
     this.radius = hex.mapSize;
@@ -46,14 +34,30 @@ hex.Map = Class.extend({
       for (var i in this._mapTiles) this._mapTiles[i].drawNeighbours();
     }
   },
+	setTransform: function() {
+		var t = this.element.attr('transform'), i;
+		if (t) {
+			t = t.replace('translate(', '')
+			t = t.replace(')', '')
+			t = t.split(',');
+			for (i = 0; i < t.length; i++) t[i] = parseInt(t[i]);
+		} else t = [0, 0];
+		this.transform = {x: t[0], y: t[1]};
+	},
+	/**
+	 * In progress, try this:
+	 * map.tessellateTo({x:  1, y: 0});
+	 * map.tessellateTo({x:  1, y:-1});
+	 * map.tessellateTo({x: -1, y: 0});
+	 * map.tessellateTo({x: -1, y: 1});
+	 * map.tessellateTo({x:  0, y: 1});
+	 * map.tessellateTo({x:  0, y:-1});
+	 */
   tessellateTo: function(pos) {
-    var self = this;
-    var tess = this.paper
+    var i, width, tessellation = this.paper
       .append('use')
       .attr('xlink:href', '#hex-map');
-    for (var i in pos) {
-      pos[i] = Math.floor(Math.abs(pos[i]/this.bounds)) * (pos[i] < 1 ? -1 : 1);
-    }
+    //for (var i in pos) pos[i] = Math.floor(Math.abs(pos[i]/this.bounds));
     width = hex.utility.trigASA(30, this.tileSize/2, 90);
     var plot = hex.utility.getNeighbourHexPlot(
       {x: 0, y: 0},
@@ -62,17 +66,29 @@ hex.Map = Class.extend({
       true
     );
     if (pos.x == 1 && pos.y == 0) {
-      tess.attr('x', plot.x - width[1]).attr('y', plot.y + width[0]*1.5);
+      tessellation
+				.attr('x', plot.x - width[1])
+				.attr('y', plot.y + width[0] * 1.5);
     } else if (pos.x == 1 && pos.y == -1) {
-      tess.attr('x', plot.x - width[1]*2).attr('y', plot.y);
+      tessellation
+				.attr('x', plot.x - width[1] * 2)
+				.attr('y', plot.y);
     } else if (pos.x == 0 && pos.y == -1) {
-      tess.attr('x', plot.x - width[1]).attr('y', plot.y - width[0]*1.5);
+      tessellation
+				.attr('x', plot.x - width[1])
+				.attr('y', plot.y - width[0] * 1.5);
     } else if (pos.x == -1 && pos.y == 0) {
-      tess.attr('x', plot.x + width[1]).attr('y', plot.y - width[0]*1.5);
+      tessellation
+				.attr('x', plot.x + width[1])
+				.attr('y', plot.y - width[0] * 1.5);
     } else if (pos.x == -1 && pos.y == 1) {
-      tess.attr('x', plot.x + width[1]*2).attr('y', plot.y);
+      tessellation
+				.attr('x', plot.x + width[1] * 2)
+				.attr('y', plot.y);
     } else if (pos.x == 0 && pos.y == 1) {
-      tess.attr('x', plot.x + width[1]).attr('y', plot.y + width[0]*1.5);
+      tessellation
+				.attr('x', plot.x + width[1])
+				.attr('y', plot.y + width[0] * 1.5);
     }
   }
 });
