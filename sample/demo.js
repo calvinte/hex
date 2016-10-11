471dc3280427e96f07d0e9987643e02933527762
Calvin Tennant <calvintennant@gmail.com>
2016-10-10 20:57:38 -0700

    restarting this project (again! haha)

---
+++ sample/demo.js
@@ -0,0 +1,7 @@
+require([
+    'd3/build/d3',
+    'underscore/underscore-min',//shim?
+], function(d3) {
+    console.log(_, d3);
+});
+
---
+++ server.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.use(express.static('src'));
+app.use(express.static('sample'));
+app.use(express.static('node_modules'));
+
+app.get('/', function(req, res) {
+    res.status(200).send(fs.readFileSync('sample/demo.html', 'utf8'));
+});
+app.listen(3000);
+
